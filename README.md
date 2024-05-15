eee5c8c3b972c139a9d93615664adc1266729772
konynunez <156865892+konynunez@users.noreply.github.com>
2024-05-15 09:20:57 -0400

    added JS functions, event listeners, edited README

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 
 [ live Site ] (https://konynunez.github.io/Capstone2/)
 
-- This code allows you to search for images using a form, view the results and load more images by clicking a button. It uses the Unsplash API to fetch the images and dosplays them in a grid with an overlay containing the image's atl description. The overlay appears when the uses hover over an image, the load more button allows users to fetch more images without leaving the page or submitting the form again (Infinite Scrolling).
+- This code allows you to search for images using a form, view the results and load more images by clicking a button. It uses the Unsplash API to fetch the images and displays them in a grid with an overlay containing the image's atl description. The overlay appears when the uses hover over an image, the load more button allows users to fetch more images without leaving the page or submitting the form again (Infinite Scrolling).
 
-- After attempting with some APIs, that did not work, this was the one that the keys are working  for me, and honestly they were very hard methods.  
+- After attempting with some APIs, that did not work, this was the one that the keys are working for me, and honestly others were very hard methods I needed long time to figure out.  
 - I watched many tutorials in Youtube, researched many pages such as:
 - Stack Overflow
 - W3 Schools 
@@ -22,8 +22,7 @@ and others
    - [x] Flavicon
    - [x] google Icons (Search Icon)
    - [x] Images Container Section
-- [x] Build a logo in Canva
-
+- [x] Build a logo in Canva (favicon)
 - [x] CSS
    - [x] Form Structure
    - [x] Search Area
@@ -35,5 +34,5 @@ and others
 - [x] Test Key (Postman)
 - [ ] JavaScript
    - [x] Link Selectors
-- [ ] 
+   - [ ] 
 - [ ] Test App in Lighthouse
--- script.js
+++ script.js
@@ -3,3 +3,63 @@ const searchForm = document.querySelector("form");
 const searchInput = document.querySelector(".search-input");
 const imagesContainer = document.querySelector(".images-container");
 const loadMoreBtn = document.querySelector(".loadMoreBtn");
+
+// function to fetch images using Unsplash API
+const fetchImages = async (query, pageNo) => {
+  try {
+    if (pageNo === 1) {
+      imagesContainer.innerHTML = '';
+    }
+
+    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.results.length > 0) {
+      data.results.forEach(photo => {
+        const imageElement = document.createElement("div");
+        imageElement.classList.add("imageDiv");
+        imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
+
+        //creating overlay
+        const overlayElement = document.createElement("div");
+        overlayElement.classList.add("overlay");
+
+        //Creating overlay text
+        const overlayText = document.createElement("h3");
+        overlayText.innerText = photo.alt_description;
+        overlayElement.appendChild(overlayText);
+
+        imageElement.appendChild(overlayElement);
+        imagesContainer.appendChild(imageElement);
+      });
+    } else {
+      imagesContainer.innerHTML = '<h2>No Image Found.</h2>';
+      if (loadMoreBtn.style.display === "block")
+        loadMoreBtn.style.display = "none";
+    }
+  } catch (error) {
+    imagesContainer.innerHTML = `<h2>Please try again later</h2>`;
+  }
+}
+
+// adding event listener to search form
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputText = searchInput.value.trim();
+  if (inputText !== "") {
+    let page = 1;
+    fetchImages(inputText, page);
+  } else {
+    imagesContainer.innerHTML = `<h2>Please enter a search query</h2>`;
+    if (loadMoreBtn.computedStyleMap().get("display") === "block") {
+      loadMoreBtn.style.display = "none";
+    }
+  }
+});
+
+//adding event listener to load more button to fetch more images
+loadMoreBtn.addEventListener('click', () => {
+  fetchImages(searchInput.value.trim(), ++page);
+});
